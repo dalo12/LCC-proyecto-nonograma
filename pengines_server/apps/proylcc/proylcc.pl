b17a2d769ab8861d3804c4483f4bcc46190a5bef
David López <dalo_vidpez@hotmail.com.ar>
2021-04-23 16:14:07 -0300

    Inicio del proyecto. Todo lo que hay es lo clonado de la cátedra.

---
+++ pengines_server/apps/proylcc/proylcc.pl
@@ -0,0 +1,31 @@
+:- module(proylcc,
+	[  
+		put/8
+	]).
+
+:-use_module(library(lists)).
+
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%
+% replace(?X, +XIndex, +Y, +Xs, -XsY)
+%
+
+replace(X, 0, Y, [X|Xs], [Y|Xs]).
+
+replace(X, XIndex, Y, [Xi|Xs], [Xi|XsY]):-
+    XIndex > 0,
+    XIndexS is XIndex - 1,
+    replace(X, XIndexS, Y, Xs, XsY).
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%
+% put(+Contenido, +Pos, +PistasFilas, +PistasColumnas, +Grilla, -GrillaRes, -FilaSat, -ColSat).
+%
+
+put(Contenido, [RowN, ColN], _PistasFilas, _PistasColumnas, Grilla, NewGrilla, 0, 0):-
+	replace(Row, RowN, NewRow, Grilla, NewGrilla),
+	(replace(Cell, ColN, _, Row, NewRow),
+	Cell == Contenido 
+		; 
+	replace(_Cell, ColN, Contenido, Row, NewRow)).
