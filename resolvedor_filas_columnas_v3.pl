ea0576d276098c50d10b1235c67728eba70370df
David López <dalo_vidpez@hotmail.com.ar>
2021-06-11 12:29:07 -0300

    Logrado que se resuelvan bien las filas y las columnas y retorne las grillas correspondientes. Todo esto está en el archivo 'resolvedor_filas_columnas_v3.pl'

--- resolvedor_filas_columnas_v3.pl
+++ resolvedor_filas_columnas_v3.pl
@@ -1,50 +1,87 @@
-% ARCHIVO DEPRECADO
-% Consultar en su lugar 'resolvedor_filas_columnas_v3.pl'
+% 
+% resolver_fila/4
+% resolver_fila(+PistasFila, +N_Fila, +Grilla, -Res)
 %
-% resolver_fila(+Pistas, +N_Fila, +Grilla, +Resto, +Modo, -Res)
-%
-% Debe ser invocado con
-% resolver_fila([lista], numero, [[grilla]], _, no_rafaga, R).
+% Deber ser invocado de la siguiente manera
+% resolver_fila([lista de naturales], n_natural, [[grilla]], R)
 %
 /**
- * Dada una grilla, resuelve una fila acorde a las pistas y la retorna en Res
- * @param Pistas Lista con las pistas
- * @param N_Fila Número de fila a resolver (0..lenght(grilla))
- * @param Grilla Grilla donde se encuentra la fila a resolver
- * @param Resto La parte restante de la grilla que no forma parte de la fila
- * @param Modo Modo de inserción de la recursión anterior. Puede ser 'rafaga' o 'no_rafaga'
- * @return Res Grilla con la fila resuelta
- */
-resolver_fila([0], 0, [[] | Grilla], Grilla, rafaga, []).
-resolver_fila([], 0, [[] | Grilla], Grilla, no_rafaga, []).
-resolver_fila(Pistas, N_Fila, [G | Grilla], _Resto, no_rafaga, [G | R]) :-
+ * Wrapper de resolver_filas_aux/4
+ * Dada una lista de números naturales como pistas, un número de fila y una grilla,
+ * devuelve la grilla resultante con la fila resuelta
+ * @param PistasFila Lista de números naturales que indican las pistas de la fila
+ * correspondiente
+ * @param N_Fila Número de la fila a resolver (0..length(grilla))
+ * @param Grilla Grilla a resolver
+ * @param Res Grilla resultante con la fila resuelta
+ */ 
+resolver_fila(PistasFila, N_Fila, [G | Grilla], [G | Res]) :-
     N_Fila > 0,
     N_Fila_Aux is N_Fila - 1,
-    resolver_fila(Pistas, N_Fila_Aux, Grilla, Resto, no_rafaga,  Res),
-    append([Res], Resto, R).
-resolver_fila(Pistas, 0, [[F | Fila] | Grilla], Resto, no_rafaga, ["X" | Res]) :-
+    resolver_fila(PistasFila, N_Fila_Aux, Grilla, Res).
+resolver_fila(PistasFila, 0, [G | Grilla], [Res | Grilla]) :-
+    resolver_fila_aux(PistasFila, G, no_rafaga, Res).
+
+%    
+% resolver_fila_aux/4
+% resolver_fila_aux(+Pistas, +Fila, +Modo, -Res)
+%
+% Debe ser invocado de la siguiente manera
+% resolver_fila_aux([lista de naturales], [Fila], no_rafaga, R).
+%
+/**
+ * Dada una fila, la resuelve acorde a las pistas y la retorna en Res
+ * @param Pistas Lista con las pistas
+ * @param Fila Fila que se intenta resolver
+ * @param Modo Modo de inserción de la recursión anterior. Puede ser 'rafaga' o 'no_rafaga'
+ * @return Res Fila resuelta
+ */
+resolver_fila_aux([0], [], rafaga, []).
+resolver_fila_aux([], [], no_rafaga, []).
+resolver_fila_aux(Pistas, [F | Fila], no_rafaga, ["X" | Res]) :-
     % F \== [], % verificar si el código sigue andando sin esta línea. De ser así, borrarla
     F \== "#",
-    resolver_fila(Pistas, 0, [Fila | Grilla], Resto, no_rafaga, Res).
-resolver_fila([P | Pistas], 0, [[F | Fila] | Grilla], Resto, no_rafaga, ["#" | Res]) :-
+    resolver_fila_aux(Pistas, Fila, no_rafaga, Res).
+resolver_fila_aux([P | Pistas], [F | Fila], no_rafaga, ["#" | Res]) :-
     F \== "X",
     P > 0,
     P_Aux is P - 1,
-    resolver_fila([P_Aux | Pistas], 0, [Fila | Grilla], Resto, rafaga, Res).
-resolver_fila([P | Pistas], 0, [[F | Fila] | Grilla], Resto, rafaga, ["#" | Res]) :-
+    resolver_fila_aux([P_Aux | Pistas], Fila, rafaga, Res).
+resolver_fila_aux([P | Pistas], [F | Fila], rafaga, ["#" | Res]) :-
     F \== "X",
     P > 0,
     P_Aux is P - 1,
-    resolver_fila([P_Aux | Pistas], 0, [Fila | Grilla], Resto, rafaga, Res).
-resolver_fila([0 | Pistas], 0, [ [F | Fila] | Grilla], Resto, rafaga, ["X" | Res]) :-
+    resolver_fila_aux([P_Aux | Pistas], Fila, rafaga, Res).
+resolver_fila_aux([0 | Pistas], [F | Fila], rafaga, ["X" | Res]) :-
     F \== "#",
-    resolver_fila(Pistas, 0, [Fila | Grilla], Resto, no_rafaga, Res).
+    resolver_fila_aux(Pistas, Fila, no_rafaga, Res).
 
 %
+% resolver_columna/4
+% resolver_columna(+PistasColumna, +N_Col, +Grilla, -Res)
+%
+% Debe ser invocado de la siguiente manera
+% resolver_columna([lista de naturales], natural, [[grilla]], R)
+%
+/**
+ * Wrapper de resolver_columna/7
+ * Dada una lista de naturales como pistas, un número de columna y una grilla,
+ * devuelve la grilla resultante con la columna resuelta
+ * @param PistasColumna Lista de números naturales que indican las pistas de la
+ * columna correspondiente
+ * @param N_Col Número de la columna a resolver (0..length(grilla[0]))
+ * @param Grilla Grilla a resolver
+ * @param Res Grilla resultante con la columna resuelta
+ */
+resolver_columna(PistasColumna, N_Col, Grilla, Res) :-
+    resolver_columna(PistasColumna, N_Col, 0, Grilla, [], no_rafaga, Res).
+
+%
+% resolver_columna/7
 % resolver_columna(+Pistas, +N_Col, +Index, +Grilla, +Cabeza, +Modo, -Res)
 %
 % Debe ser invocado con
-% resolver_columna([lista], numero, 0, [[grilla]], [], no_rafaga, R).
+% resolver_columna([lista de naturales], n_natural, 0, [[grilla]], [], no_rafaga, R).
 %
 /**
  * Dada una grilla, resuelve una columna acorde a las pistas y la retorna en Res
